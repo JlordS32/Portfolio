fd98dbd271dab10effc04833e412a51492b6f977
JlordS32 <jlordsty128@gmail.com>
2025-11-21 14:37:41 +1100

    added home page, installed react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-function App() {
+
+const App = () => {
    return (
       <div className="App">
-         {" "}
-         <h1 className="text-3xl font-bold underline bg-indigo-500 hover:bg-red-300 transition ease-in-out">
-            Hello world!
-         </h1>
-         <div>
-            Lorem ipsum,{" "}
-            <span className="decoration-green-600 text-4xl text-red-400 underline">
-               dolor sit
-            </span>{" "}
-            amet consectetur adipisicing elit. Magni, ex.
-         </div>
-         <div>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-            iste blanditiis, dolorem excepturi adipisci repellendus voluptate
-            iusto ab illo recusandae veritatis inventore atque porro magnam quos
-            voluptatum eaque error, expedita laudantium! Numquam, quaerat earum,
-            ad{" "}
-            <span className="text-red-900/90 text-4xl">
-               cupiditate consequuntur sunt sit inventore distinctio laborum
-            </span>{" "}
-            at itaque dolore pariatur rem iure suscipit aliquam dignissimos. Ad
-            dignissimos eum fugiat laboriosam? A, ipsum temporibus corporis
-            sequi modi obcaecati eos minima doloribus, reiciendis deserunt
-            praesentium! Quibusdam dicta tempore asperiores, sit repudiandae
-            incidunt placeat iure. Earum, minima vero. Culpa animi assumenda
-            error ex ab in reiciendis unde nam incidunt odio, delectus, maiores,
-            natus officia quae illo consectetur?
-         </div>
+         
       </div>
    );
-}
+};
 
 export default App;
