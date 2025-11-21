7f68b2cba37c0d37ce4140a75da9d628eb96affd
JlordS32 <jlordsty128@gmail.com>
2025-11-21 15:40:32 +1100

    added not found page

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // PAGES
 import Home from "./pages/Home.tsx";
+import NotFound from "./pages/NotFound.tsx";
 
 // STYLES
 import "./index.css";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const router = createBrowserRouter([
+   { path: "/", element: <Home /> },
+   { path: "*", element: <NotFound /> },
+]);
 
 createRoot(document.getElementById("root")!).render(
    <StrictMode>
