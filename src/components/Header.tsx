f75fd30ce3825113a5a2098648bafc1b950f16c0
JlordS32 <jlordsty128@gmail.com>
2025-11-21 18:14:57 +1100

    boilerplate

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+   return (
+      <header>
+         <div className="logo">{"<SS />"}</div>
+         <div className="options"></div>
+      </header>
+   );
+};
+
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,39 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 // PAGES
 import Home from "./pages/Home.tsx";
 import NotFound from "./pages/NotFound.tsx";
 
+// COMPONENTS
+import Header from "./components/Header.tsx";
+import Footer from "./components/Footer.tsx";
+
 // STYLES
 import "./index.css";
 
+// LAYOUT
+const RootLayout = () => {
+   return (
+      <>
+         <Header />
+         <main>
+            <Outlet />
+         </main>
+         <Footer />
+      </>
+   );
+};
+
+// ROUTING CONFIG
 const router = createBrowserRouter([
-   { path: "/", element: <Home /> },
-   { path: "*", element: <NotFound /> },
+   {
+      path: "/",
+      Component: RootLayout,
+      children: [{ index: true, Component: Home }],
+   },
+   { path: "*", Component: NotFound },
 ]);
 
 createRoot(document.getElementById("root")!).render(
