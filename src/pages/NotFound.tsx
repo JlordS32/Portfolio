76c40059bae0b8f524d097469bc09912f8d29a35
JlordS32 <jlordsty128@gmail.com>
2025-11-27 19:32:01 +1100

    added not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,19 @@
+import Button from "@/components/Button";
+import { useNavigate } from "react-router-dom";
+
 const NotFound = () => {
+   const navigate = useNavigate();
+
    return (
-      <div className="text-center">
-         <h1>404 Page not found {":("}</h1>
+      <div className="text-center min-h-[82vh] flex justify-center align-center">
+         <div className="self-center">
+            <h1>404 - Page Not Found {":("}</h1>
+            <p className="pt-6">Oops! We couldn't find that page.</p>
+
+            <Button className="btn mt-12" onClick={() => navigate(-1)}>
+               Go Back
+            </Button>
+         </div>
       </div>
    );
 };
